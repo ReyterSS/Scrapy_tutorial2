910068c5c7fdbfdfc03da7fa135017dd73a6c056
ReyterSS <52965191+ReyterSS@users.noreply.github.com>
2022-09-23 18:25:28 +0300

    eth2

--- tutorial/tutorial/spiders/quotes.py
+++ tutorial/tutorial/spiders/quotes.py
@@ -3,8 +3,7 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
     start_urls = [
-        'https://quotes.toscrape.com/page/1/',
-        'https://quotes.toscrape.com/page/2/',
+        'https://quotes.toscrape.com/page/1/'
     ]
 
     def parse(self, response):
@@ -14,3 +13,7 @@ class QuotesSpider(scrapy.Spider):
                 'author': quote.css('small.author::text').get(),
                 'tags': quote.css('div.tags a.tag::text').getall()
             }
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
